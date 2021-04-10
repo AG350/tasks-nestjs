116588b3a4018c20b3b88836099687290754e4cb
Alejandro Galeano <ale.gb32@gmail.com>
2021-04-10 17:43:24 -0300

    Validated the update and delete task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
-import { stat } from 'node:fs';
 
 @Injectable()
 export class TasksService {
@@ -29,11 +28,17 @@ export class TasksService {
     }
     
     getTaskById(id: string){
-        return this.tasks.find(task=>task.id === id)
+      const found = this.tasks.find(task=>task.id === id);
+
+      if(!found){
+          throw new NotFoundException(`Task with ID '${id}' not found`);
+      }
+
+      return found;
     }
 
     createTask(createTaskDto: CreateTaskDto): Task{
-        const {title, description} = createTaskDto
+        const {title, description} = createTaskDto;
         const task: Task = {
             id: uuid(),
             title,
@@ -46,7 +51,8 @@ export class TasksService {
     }
 
     deleteTaskById(id: string): void{
-        this.tasks = this.tasks.filter(task => task.id !== id)
+        const found = this.getTaskById(id)
+        this.tasks = this.tasks.filter(task => task.id !== found.id);
     }
 
     updateTaskStatus(id: string, status: TaskStatus): Task{
