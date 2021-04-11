9ca3d3e9bb50af4af74da61dea613163d28a5804
Alejandro Galeano <ale.gb32@gmail.com>
2021-04-11 11:10:17 -0300

    Refactored the getTask controller, service and created a costum query in repository to get task filtered or not

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,14 +11,10 @@ import { TasksService } from './tasks.service';
 export class TasksController {
     constructor(private tasksService: TasksService) {}
 
-    // @Get()
-    // getTasks(@Query(ValidationPipe) filterDto: GetTaskFilterDto): Task[] {
-    //     if(Object.keys(filterDto).length){
-    //         return this.tasksService.getTasksWithFilters(filterDto)
-    //     } else{
-    //         return this.tasksService.getAllTasks()
-    //     }
-    // }
+    @Get()
+    getTasks(@Query(ValidationPipe) filterDto: GetTaskFilterDto): Promise<Task[]> {
+       return this.tasksService.getTasks(filterDto);
+    }
 
     @Post()
     @UsePipes(ValidationPipe)
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -14,24 +14,11 @@ export class TasksService {
         private taskRepository: TaskRepository
     ) {}
 
-    // getAllTasks(): Task[] {
-    //     return this.tasks;
-    // }
+    async getTasks(filterDto: GetTaskFilterDto): Promise<Task[]> {
+        return this.taskRepository.getTasks(filterDto);
+    }
 
-    // getTasksWithFilters(filterDto: GetTaskFilterDto): Task[] {
-    //     const { status, search} = filterDto;
-    //     let tasks = this.getAllTasks();
-    //     if(status) {
-    //         tasks = tasks.filter(task => task.status === status)
-    //     }
-    //     if(search) {
-    //         tasks = tasks.filter(task => 
-    //             task.title.includes( search) ||
-    //             task.description.includes( search),
-    //         );
-    //     }
-    //     return tasks;
-    // }
+    
     async getTaskById(id:number): Promise<Task> {
         const found = await this.taskRepository.findOne(id);
         if(!found){
