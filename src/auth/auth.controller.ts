9dcbe6f1e48d89e883c0cef49d544e6349894344
Alejandro Galeano <ale.gb32@gmail.com>
2021-04-11 14:00:05 -0300

    Created an auth DTO, singUp controller and service, set it on the DB

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private authServices: AuthService,
+    ) {}
+
+    @Post('/singup')
+    singUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.authServices.singUp(authCredentialsDto)
+    }
+}
